e4ff37a69000cb36cdebef506a12e17861abc381
Guilherme <guilinard2003@gmail.com>
2025-11-08 16:59:38 -0300

    adicionar cabeçalho do grupo

--- src/main/java/com/empresa/reagentes/ReagentesApiApplication.java
+++ src/main/java/com/empresa/reagentes/ReagentesApiApplication.java
@@ -3,6 +3,14 @@ package com.empresa.reagentes;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+// Projeto DDD - Grupo 2ESPH
+// Integrantes:
+// Guilherme Linard - RM555768
+// Lucas Vasquez - RM555159
+// Lucas Rodrigues - RM556323
+// Laura Souza - RM556320
+// Ali Andrea - RM558052
+
 @SpringBootApplication
 public class ReagentesApiApplication {
     public static void main(String[] args) {
